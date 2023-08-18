ecb15aea49c95e6d753c1b0d676f5cb64cff70e5
g.ferrer <gustavo.ferrer@totvs.com.br>
2023-08-17 23:10:54 -0300

    feat: initial structure with nextjs 13

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import DashboardTemplate from "@/templates/DashboardTemplate/DashboardTemplate.core";
+import React from "react";
+
+export default function Index() {
+  return <DashboardTemplate />;
+}
